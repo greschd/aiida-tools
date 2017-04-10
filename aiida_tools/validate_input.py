563ae3779976e8191a6f5b9d9e271f7727393c72
Dominik Gresch <greschd@gmx.ch>
2017-04-10 10:51:56 +0200

    Run input validation BEFORE start step, add message after validation

--- aiida_tools/validate_input.py
+++ aiida_tools/validate_input.py
@@ -16,7 +16,7 @@ from aiida.common.exceptions import InputValidationError
 
 from decorator import decorator
 
-class Parameter(object):
+class _Parameter(object):
     def __init__(self, type, required):
         self.type = type
         self.required = bool(required)
@@ -28,7 +28,7 @@ def _init_params(cls):
 def parameter(name, type=None, required=True):
     def inner(cls):
         _init_params(cls)
-        cls._params[name] = Parameter(type=type, required=required)
+        cls._params[name] = _Parameter(type=type, required=required)
         return cls
     return inner
 
@@ -45,7 +45,6 @@ def inherit_parameters(workflow, ignore=()):
 def validate_input(cls):
     @decorator
     def _run_validate(func, self, *args, **kwargs):
-        func(self, *args, **kwargs)
         params = self.get_parameters()
         valid_params = getattr(cls, '_params', dict())
 
@@ -59,5 +58,9 @@ def validate_input(cls):
                     continue
             if not isinstance(r, val.type):
                 raise InputValidationError("Input parameter '{}' is of invalid type '{}'; should be '{}'.".format(name, type(r), val.type))
+        self.append_to_report('Input validation passed. Starting workflow with the following parameters:' + ''.join(
+            ['\n    {}: {}'.format(key, val) for key, val in sorted(self.get_parameters().items())]
+        ))
+        return func(self, *args, **kwargs)
     cls.start = _run_validate(cls.start.__func__)
     return cls
