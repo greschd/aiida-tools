af99781aca8f2ea5a8199958f385aff1e0180a43
Dominik Gresch <greschd@gmx.ch>
2018-03-06 16:18:35 +0100

    Partial update for workflows AiiDA branch

--- aiida_tools/workchain_inputs.py
+++ aiida_tools/workchain_inputs.py
@@ -23,19 +23,19 @@ def get_fullname(cls_obj):
 @get_fullname.register(str)
 def _(cls_name):
     return Str(cls_name)
+#
+#
+# @get_fullname.register(Str)
+# def _(cls_name):
+#     return cls_name
 
-
-@get_fullname.register(Str)
-def _(cls_name):
-    return cls_name
-
-@export
-def load_class(cls_name):
-    return CLASS_LOADER.load_class(cls_name.value)
+# @export
+# def load_class(cls_name):
+#     return CLASS_LOADER.load_class(cls_name.value)
 
 #: Keyword arguments to be passed to ``spec.input`` for serializing an input which is a class / workchain into a string.
 WORKCHAIN_INPUT_KWARGS = {
     'valid_type': Str,
     'serialize_fct': get_fullname,
-    'deserialize_fct': load_class
+    # 'deserialize_fct': load_class
 }
