475a8a4746f97e370b50496420269e8fca4e61a4
Dominik Gresch <a-dogres@microsoft.com>
2019-09-04 13:03:19 -0700

    Remove input validator. This should now be done through the spec.

--- aiida_tools/__init__.py
+++ aiida_tools/__init__.py
@@ -9,8 +9,7 @@ This module contains general helper functions to aid in developing AiiDA calcula
 
 __version__ = '0.1.1'
 
-from ._input_validator import get_input_validator
 from ._check_workchain_step import *
 from . import workchain_inputs
 
-__all__ = ['workchain_inputs'] + _input_validator.__all__ + _check_workchain_step.__all__
+__all__ = ['workchain_inputs'] + _check_workchain_step.__all__
