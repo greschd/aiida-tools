a35b4351a5b89a2f406031585ae001ecb6421cf5
Dominik Gresch <a-dogres@microsoft.com>
2019-11-22 21:16:42 +0100

    Add 'get_outputs_dict' function.

--- aiida_tools/__init__.py
+++ aiida_tools/__init__.py
@@ -2,14 +2,15 @@
 
 # © 2017-2019, ETH Zurich, Institut für Theoretische Physik
 # Author: Dominik Gresch <greschd@gmx.ch>
-
 """
 This module contains general helper functions to aid in developing AiiDA calculations and workchains.
 """
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
+from ._get_outputs_dict import *
 from ._check_workchain_step import *
 from . import process_inputs
 
-__all__ = ['process_inputs'] + _check_workchain_step.__all__
+__all__ = (['process_inputs'] + _check_workchain_step.__all__ +
+           _get_outputs_dict.__all__)
