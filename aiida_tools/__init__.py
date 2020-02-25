8d43e9c34be1ba2ed184c5c92528e8c862af9d8c
Dominik Gresch <a-dogres@microsoft.com>
2020-02-25 12:19:48 +0100

    Update version number to v0.3.3

--- aiida_tools/__init__.py
+++ aiida_tools/__init__.py
@@ -6,7 +6,7 @@
 This module contains general helper functions to aid in developing AiiDA calculations and workchains.
 """
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 from ._get_outputs_dict import *
 from ._check_workchain_step import *
